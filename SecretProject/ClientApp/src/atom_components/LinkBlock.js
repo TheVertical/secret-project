1cd89735115d1a231c6db81b6d997bcc469adfd8
GenaSidorov <gennadiy_sidorov_01@mail.ru>
2020-11-23 22:54:03 +0500

    Новые визуальные React-элементы (атомарные)

---
+++ SecretProject/ClientApp/src/atom_components/LinkBlock.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+// Здесь будут состояния
+
+
+
+//Здесь будут функции(внешние)
+
+
+
+
+//Здесь будет главная функция экспорта 
+const linkblock=(props)=>{
+
+    //Здесь будут функции(внутренние)
+
+return(
+<div>
+    <h1>LinkBlock</h1>
+</div>
+);
+
+}
+
+
+
+export default linkblock
---
+++ SecretProject/ClientApp/src/atom_components/Picture.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+// Здесь будут состояния
+
+
+
+//Здесь будут функции(внешние)
+
+
+
+
+//Здесь будет главная функция экспорта 
+const picture=(props)=>{
+
+    //Здесь будут функции(внутренние)
+
+return(
+<div>
+<h1>picture</h1>
+</div>
+);
+
+}
+
+
+
+export default picture
