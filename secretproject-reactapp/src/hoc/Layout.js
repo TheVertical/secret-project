1aab42a3d8c59f914d4411d853a138d60400bfbb
Gena Sidorov <genandiy_sidorov_01@mail.ru>
2020-11-26 21:37:30 +0500

    Второй пробный коммит

--- secretproject-reactapp/src/hoc/Layout.js
+++ secretproject-reactapp/src/hoc/Layout.js
@@ -6,11 +6,11 @@ import React, { Children } from 'react';
 //    Picture,
 //    Radio
 // } from "../components/"
-import Button from "../atom_components/Button"
-import CheckBox from "../atom_components/CheckBox"
-import LinkBlock from "../atom_components/LinkBlock"
-import Picture from "../atom_components/Picture"
-import Radio  from "../atom_components/Radio"
+import Button from "../basedComponents/Button"
+import CheckBox from "../basedComponents/CheckBox"
+import LinkBlock from "../basedComponents/LinkBlock"
+import Picture from "../basedComponents/Picture"
+import Radio  from "../basedComponents/Radio"
 
 class Layout extends React.Component{
 
