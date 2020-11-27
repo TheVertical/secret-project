b1cf00be3946a7dd42c5dbf53249faf995cc561c
Danila <d.PotapovAnt@yandex.ru>
2020-11-27 21:07:42 +0300

    Создал визуальный редактор для виз. элементов

---
+++ SecretProject.VisualElements/IVisualRedactor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretProject.VisualElements
+{
+    public interface IVisualRedactor
+    {
+        object GetBackbone();
+        object GetFormattedVisualElement(object element);
+        object GetFormattedVisualElement(IEnumerable<object> element);
+        object GetAllVisualElements();
+    }
+
+    public interface IVisualRedactor<TResult>
+    {
+        TResult GetBackbone();
+        TResult GetFormattedVisualElement(object element);
+        TResult GetFormattedVisualElement(IEnumerable<object> element);
+        TResult GetAllVisualElements();
+    }
+}
