7e2b287678fcb558b2250a65e71765faa764efbd
Danila <d.PotapovAnt@yandex.ru>
2020-11-29 01:00:29 +0300

    Добавил визуальный элемнет банер

---
+++ SecretProject.VisualElements/Elements/Banner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SecretProject.VisualElements.Elements
+{
+    public class Banner : IVisualElement
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotMapped]
+        public string Type => this.GetType().Name;
+        public string Link { get; set; }
+        public string ImageSource { get; set; }
+
+    }
+}
