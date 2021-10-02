1e5ca245635f7cda653fcaa9971ca5a63ef543b1
DarkSoul <danila351960@yandex.ru>
2021-10-03 01:40:49 +0300

    SP-# service was renamed to right

---
+++ SecretProject.BusinessProject/Services/Encription/EncryptionService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecretProject.BusinessProject.Services.Encription
+{
+    public class EncryptionService
+    {
+        private Encoding encoding;
+        public EncryptionService(Encoding encoding = null)
+        {
+            this.encoding = encoding ?? Encoding.ASCII;
+        }
+        public string Encrypt(string saltedStringForEncryption)
+        {
+            string encryptedString = String.Empty;
+            using (var shaProvider = new SHA256CryptoServiceProvider())
+            {
+                var bytes = encoding.GetBytes(saltedStringForEncryption);
+                shaProvider.Initialize();
+
+                var encryptedBytes = shaProvider.ComputeHash(bytes);
+                encryptedString = Encoding.UTF8.GetString(encryptedBytes);
+            }
+            return encryptedString;
+        }
+    }
+}
